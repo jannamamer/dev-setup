f8c988ae4129e9bf688ba3ceb71858660ca84cd2
Janna Dela Cruz <53164350+jannamamer@users.noreply.github.com>
2025-09-04 11:21:57 +1200

    Add more git alias

--- zshrc/zshrc.d/aliases.zsh
+++ zshrc/zshrc.d/aliases.zsh
@@ -5,14 +5,16 @@ alias v='nvim'
 
 # Git shortcuts
 alias ga='git add'
-alias gc="git commit"
+alias gc="git commit "
 alias gco="git checkout"
+alias gd="git diff"
 alias gl="git pull"
-alias glg="git log"
+alias glg="git log --show-signature"
 alias gp='git push'
 alias gpnb='git push --set-upstream origin $(git_current_branch)'
 alias gr='git rebase'
 alias gs="git status --porcelain"
+alias gu="git reset"
 
 # Docker aliases
 alias dc=docker compose
